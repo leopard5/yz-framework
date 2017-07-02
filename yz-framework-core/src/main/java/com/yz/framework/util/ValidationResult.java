ab73d82fbd1cd95f4a4349346df98c06978cc142
qiyazhong <qiyazhong@163.com>
2017-07-02 22:45:01 +0800

    ValidationUtil ValidationResult

---
+++ yz-framework-core/src/main/java/com/yz/framework/util/ValidationResult.java
@@ -0,0 +1,33 @@
+package com.yz.framework.util;
+
+import java.util.Map;
+
+public class ValidationResult {
+    //校验结果是否有错
+    private boolean hasErrors;
+
+    //校验错误信息
+    private Map<String, String> errorMsg;
+
+    public boolean isHasErrors() {
+        return hasErrors;
+    }
+
+    public void setHasErrors(boolean hasErrors) {
+        this.hasErrors = hasErrors;
+    }
+
+    public Map<String, String> getErrorMsg() {
+        return errorMsg;
+    }
+
+    public void setErrorMsg(Map<String, String> errorMsg) {
+        this.errorMsg = errorMsg;
+    }
+
+    @Override
+    public String toString() {
+        return "ValidationResult [hasErrors=" + hasErrors + ", errorMsg="
+                + errorMsg + "]";
+    }
+}
