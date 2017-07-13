3ef20b62ad64a0c4f5c582acc301fca48d9cf849
qiyazhong <qiyazhong@163.com>
2017-07-13 10:23:47 +0800

    dubbo filter

--- yz-framework-dubbo/src/main/java/com/yz/framework/dubbo/InvokeLogWoker.java
+++ yz-framework-dubbo/src/main/java/com/yz/framework/dubbo/InvokeLogWoker.java
@@ -1,13 +1,3 @@
-/**
- * yz-gc-service 
- * InvokeLogWoker.java 
- * com.yz.gc.service.dubbo 
- * TODO  
- * @author yazhong.qi
- * @date   2015年10月26日 下午1:40:43 
- * @version   1.0
- */
-
 package com.yz.framework.dubbo;
 
 import com.alibaba.dubbo.rpc.Result;
@@ -16,106 +6,91 @@ import com.alibaba.fastjson.JSONObject;
 import com.yz.framework.bizlogging.BizLogger;
 import com.yz.framework.logging.Logger;
 
-/**
- * ClassName:InvokeLogWoker <br/>
- * Function: TODO ADD FUNCTION. <br/>
- * Reason: TODO ADD REASON. <br/>
- * Date: 2015年10月26日 下午1:40:43 <br/>
- * 
- * @author yazhong.qi
- * @version 1.0
- * @since JDK 1.7
- * @see
- */
 public class InvokeLogWoker implements Runnable {
 
-	// private static final int SUCESS_STATUS = 10000;
-	private static final int SYS_ERROR = 10001;
+    // private static final int SUCESS_STATUS = 10000;
+    private static final int SYS_ERROR = 10001;
 
-	private Object[] arguments;
-	private String api;
-	private String serviceName;
-	private String serverIP;
-	private String from;
-	private String invokerIP;
-	private BizLogger bizLogger;
-	private long requestTime;
-	private long returnTime;
-	private Result result;
-	private Throwable exception;
+    private Object[] arguments;
+    private String api;
+    private String serviceName;
+    private String serverIP;
+    private String from;
+    private String invokerIP;
+    private BizLogger bizLogger;
+    private long requestTime;
+    private long returnTime;
+    private Result result;
+    private Throwable exception;
 
-	public InvokeLogWoker(
-			final Object[] arguments,
-			final String api,
-			final String serviceName,
-			final String serverIP,
-			final String from,
-			final String invokerIP,
-			final long requestTime,
-			final long returnTime,
-			final Result result,
-			final Throwable ex,
-			final BizLogger bizLogger
+    public InvokeLogWoker(
+            final Object[] arguments,
+            final String api,
+            final String serviceName,
+            final String serverIP,
+            final String from,
+            final String invokerIP,
+            final long requestTime,
+            final long returnTime,
+            final Result result,
+            final Throwable ex,
+            final BizLogger bizLogger
 
-	)
-	{
-		this.arguments = arguments;
-		this.api = api;
-		this.serviceName = serviceName;
-		this.serverIP = serverIP;
-		this.from = from;
-		this.invokerIP = invokerIP;
-		this.requestTime = requestTime;
-		this.returnTime = returnTime;
-		this.exception = ex;
-		this.result = result;
-		this.bizLogger = bizLogger;
-	}
+    ) {
+        this.arguments = arguments;
+        this.api = api;
+        this.serviceName = serviceName;
+        this.serverIP = serverIP;
+        this.from = from;
+        this.invokerIP = invokerIP;
+        this.requestTime = requestTime;
+        this.returnTime = returnTime;
+        this.exception = ex;
+        this.result = result;
+        this.bizLogger = bizLogger;
+    }
 
-	private static final Logger LOGGER = Logger.getLogger(InvokeLogWoker.class);
+    private static final Logger LOGGER = Logger.getLogger(InvokeLogWoker.class);
 
-	@Override
-	public void run() {
-		try {
-			String message = JSON.toJSONString(arguments);
-			int status = -1;
-			String returnMessage = null;
-			String returnData = null;
-			if (result == null) {
-				if (exception != null) {
-					returnMessage = LOGGER.getDetailMessage(exception);
-				}
-				else {
-					returnMessage = "没有异常，没有返回值，不可能，请检查代码";
-				}
-			}
-			else {
-				Object bizResult = result.getValue();
-				if (bizResult == null) {
-					Throwable bizException = result.getException();
-					if (bizException != null) {
-						returnMessage = bizException.toString();
-						returnData = LOGGER.getDetailMessage(bizException);
-					}
-				}
-				else {
-					JSONObject message2 = (JSONObject) JSON.toJSON(bizResult);
-					status = message2.getIntValue("status");
-					returnMessage = message2.getString("message");
-					if (status == SYS_ERROR) {
-						if (exception != null) {
-							returnData = LOGGER.getDetailMessage(exception);
-						}
-					}
-				}
-			}
-			bizLogger.logInvoke(api, message, from, "", requestTime, returnTime, status,
-					returnMessage, returnData, invokerIP, serverIP, serviceName);
+    @Override
+    public void run() {
+        try {
+            String message = JSON.toJSONString(arguments);
+            int status = -1;
+            String returnMessage = null;
+            String returnData = null;
+            if (result == null) {
+                if (exception != null) {
+                    returnMessage = LOGGER.getDetailMessage(exception);
+                } else {
+                    returnMessage = "没有异常，没有返回值，不可能，请检查代码";
+                }
+            } else {
+                Object bizResult = result.getValue();
+                if (bizResult == null) {
+                    Throwable bizException = result.getException();
+                    if (bizException != null) {
+                        returnMessage = bizException.toString();
+                        returnData = LOGGER.getDetailMessage(bizException);
+                    }
+                } else {
+                    JSONObject message2 = (JSONObject) JSON.toJSON(bizResult);
+                    status = message2.getIntValue("status");
+                    returnMessage = message2.getString("message");
+                    if (status == SYS_ERROR) {
+                        if (exception != null) {
+                            returnData = LOGGER.getDetailMessage(exception);
+                        }
+                    }
+                }
+            }
+            bizLogger.logInvoke(api, message, from, "", requestTime, returnTime, status,
+                    returnMessage, returnData, invokerIP, serverIP, serviceName);
 
-		} catch (Exception e) {
-			LOGGER.error("run", "记录日志错误", e);
-		}
+        } catch (Exception e) {
+            LOGGER.error("run", "记录日志错误", e);
+        }
 
-	}
+    }
 
 }
--- yz-framework-dubbo/src/main/java/com/yz/framework/dubbo/InvokeLogger.java
+++ yz-framework-dubbo/src/main/java/com/yz/framework/dubbo/InvokeLogger.java
@@ -1,20 +1,4 @@
-/**
- * yz-gc-service 
- * InvokeLogFilter.java 
- * com.yz.gc.service.dubbo 
- * TODO  
- * @author yazhong.qi
- * @date   2015年10月22日 下午6:20:15 
- * @version   1.0
- */
-
 package com.yz.framework.dubbo;
-
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-
-import org.springframework.beans.factory.DisposableBean;
-import org.springframework.beans.factory.InitializingBean;
 
 import com.alibaba.dubbo.common.URL;
 import com.alibaba.dubbo.rpc.Invocation;
@@ -24,72 +8,67 @@ import com.alibaba.dubbo.rpc.RpcException;
 import com.yz.framework.bizlogging.BizLogger;
 import com.yz.framework.logging.Logger;
 import com.yz.framework.util.ApplicationContextUtil;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
 
-/**
- * ClassName:InvokeLogFilter <br/>
- * Function: TODO ADD FUNCTION. <br/>
- * Reason: TODO ADD REASON. <br/>
- * Date: 2015年10月22日 下午6:20:15 <br/>
- * 
- * @author yazhong.qi
- * @version 1.0
- * @since JDK 1.7
- * @see
- */
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class InvokeLogger implements InitializingBean, DisposableBean {
-	private static final Logger LOGGER = Logger.getLogger(InvokeLogger.class);
-	private static BizLogger bizLogger;
+    private static final Logger LOGGER = Logger.getLogger(InvokeLogger.class);
+    private static BizLogger bizLogger;
 
-	private final static ExecutorService EXECUTOR_SERVICE;
-	static {
-		EXECUTOR_SERVICE = Executors.newSingleThreadExecutor();
-	}
+    private final static ExecutorService EXECUTOR_SERVICE;
 
-	public static void logInvoke(
-			Invoker<?> invoker,
-			Invocation invocation,
-			final long requestTime,
-			final Result result,
-			final String invokerHostName,
-			final String invokerIP,
-			final RpcException rpcException
-			) {
+    static {
+        EXECUTOR_SERVICE = Executors.newSingleThreadExecutor();
+    }
 
-		final Object[] arguments = invocation.getArguments();
-		final String api = invoker.getInterface().getName() + "." + invocation.getMethodName();
-		URL node = invoker.getUrl();
-		final String serviceName = invoker.getUrl().getParameter("application");
-		final String serverIP = node.getHost();
-		final long returnTime = System.currentTimeMillis();
+    public static void logInvoke(
+            Invoker<?> invoker,
+            Invocation invocation,
+            final long requestTime,
+            final Result result,
+            final String invokerHostName,
+            final String invokerIP,
+            final RpcException rpcException
+    ) {
 
-		Throwable lastException = rpcException == null ? LOGGER.getLastException() : rpcException;
-		InvokeLogWoker invokeLogWoker = new InvokeLogWoker(
-				arguments,
-				api,
-				serviceName,
-				serverIP,
-				invokerHostName,
-				invokerIP,
-				requestTime,
-				returnTime,
-				result,
-				lastException,
-				bizLogger);
+        final Object[] arguments = invocation.getArguments();
+        final String api = invoker.getInterface().getName() + "." + invocation.getMethodName();
+        URL node = invoker.getUrl();
+        final String serviceName = invoker.getUrl().getParameter("application");
+        final String serverIP = node.getHost();
+        final long returnTime = System.currentTimeMillis();
 
-		EXECUTOR_SERVICE.execute(invokeLogWoker);
+        Throwable lastException = rpcException == null ? LOGGER.getLastException() : rpcException;
+        InvokeLogWoker invokeLogWoker = new InvokeLogWoker(
+                arguments,
+                api,
+                serviceName,
+                serverIP,
+                invokerHostName,
+                invokerIP,
+                requestTime,
+                returnTime,
+                result,
+                lastException,
+                bizLogger);
 
-	}
+        EXECUTOR_SERVICE.execute(invokeLogWoker);
 
-	@Override
-	public void destroy() throws Exception {
-		EXECUTOR_SERVICE.shutdown();
-	}
+    }
 
-	@Override
-	public void afterPropertiesSet() throws Exception {
+    @Override
+    public void destroy() throws Exception {
+        EXECUTOR_SERVICE.shutdown();
+    }
 
-		bizLogger = ApplicationContextUtil.getBean(BizLogger.class);
+    @Override
+    public void afterPropertiesSet() throws Exception {
 
-	}
+        bizLogger = ApplicationContextUtil.getBean(BizLogger.class);
+
+    }
 
 }
