a45cfa5287d2db44341b2c2c8b8b9c5bc9898701
qiyazhong <qiyazhong@163.com>
2017-10-12 20:45:45 +0800

    redis lock timeout setting stock decrease script

---
+++ yz-framework-core/src/main/resources/mutil_stock_decrease.lua
@@ -0,0 +1,25 @@
+local currentChannelStock = redis.call('GET',KEYS[1])
+local personRemainingcount = redis.call('GET',KEYS[2])
+if currentChannelStock then
+	local currentStockNum = tonumber(currentChannelStock)
+	local increment = tonumber(ARGV[1])
+	local perPersonLimit = tonumber(ARGV[2])
+	if personRemainingcount then
+		if currentStockNum > 0 and (currentStockNum - increment) >= 0 then
+			if personRemainingcount > 0 and (personRemainingcount - increment) >= 0 then
+				redis.call('INCRBY', KEYS[1], -(increment))
+				redis.call('INCRBY', KEYS[2], -(increment))
+ 			end
+			return 'LIMIT'
+		end
+		return 'NO'
+	else
+		if currentStockNum > 0 and (currentStockNum - increment) >= 0 then
+			redis.call('INCRBY', KEYS[1], -(increment))
+			redis.call('SET', KEYS[2], perPersonLimit - increment)
+			return 'OK'
+		end
+		return 'NO'
+	end
+end
+return "NOKEY"
