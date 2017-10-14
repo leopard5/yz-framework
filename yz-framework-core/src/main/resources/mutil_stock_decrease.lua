1e2fba3f9ddc8f67f050a0288c86466d66a4dd9e
qiyazhong <qiyazhong@163.com>
2017-10-14 21:32:12 +0800

    stock decrease lua script modify

--- yz-framework-core/src/main/resources/mutil_stock_decrease.lua
+++ yz-framework-core/src/main/resources/mutil_stock_decrease.lua
@@ -4,12 +4,14 @@ if currentChannelStock then
 	local currentStockNum = tonumber(currentChannelStock)
 	local increment = tonumber(ARGV[1])
 	local perPersonLimit = tonumber(ARGV[2])
+	local ttl = tonumber(ARGV[3])
 	if personRemainingcount then
 		if currentStockNum > 0 and (currentStockNum - increment) >= 0 then
 			if personRemainingcount > 0 and (personRemainingcount - increment) >= 0 then
 				redis.call('INCRBY', KEYS[1], -(increment))
 				redis.call('INCRBY', KEYS[2], -(increment))
- 			end
+				return 'OK'
+			end
 			return 'LIMIT'
 		end
 		return 'NO'
@@ -17,6 +19,7 @@ if currentChannelStock then
 		if currentStockNum > 0 and (currentStockNum - increment) >= 0 then
 			redis.call('INCRBY', KEYS[1], -(increment))
 			redis.call('SET', KEYS[2], perPersonLimit - increment)
+			redis.call('EXPIRE', KEYS[2], ttl)
 			return 'OK'
 		end
 		return 'NO'
