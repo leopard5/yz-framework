ca839923ad35a5339f3eb57e0d08f35258239252
qiyazhong <qiyazhong@163.com>
2018-04-29 10:15:10 +0800

    coupon_stock_decrease.lua person remaining count check first

--- yz-framework-core/src/main/resources/coupon_stock_decrease.lua
+++ yz-framework-core/src/main/resources/coupon_stock_decrease.lua
@@ -1,21 +1,21 @@
-local currentChannelStock = redis.call('GET',KEYS[1])
-local personRemainingcount = redis.call('GET',KEYS[2])
+local currentChannelStock = redis.call('GET', KEYS[1])
+local personRemainingcount = redis.call('GET', KEYS[2])
 if currentChannelStock then
 	local currentStockNum = tonumber(currentChannelStock)
 	local increment = tonumber(ARGV[1])
 	local perPersonLimit = tonumber(ARGV[2])
 	local ttl = tonumber(ARGV[3])
 	if personRemainingcount then
-		if currentStockNum > 0 and (currentStockNum - increment) >= 0 then
-			personRemainingcount = tonumber(personRemainingcount)
-			if personRemainingcount > 0 and (personRemainingcount - increment) >= 0 then
+		personRemainingcount = tonumber(personRemainingcount)
+		if personRemainingcount > 0 and (personRemainingcount - increment) >= 0 then
+			if currentStockNum > 0 and (currentStockNum - increment) >= 0 then
 				redis.call('INCRBY', KEYS[1], -(increment))
 				redis.call('INCRBY', KEYS[2], -(increment))
 				return 'OK'
- 			end
-			return 'LIMIT'
+			end
+			return 'NO'
 		end
-		return 'NO'
+		return 'LIMIT'
 	else
 		if currentStockNum > 0 and (currentStockNum - increment) >= 0 then
 			redis.call('INCRBY', KEYS[1], -(increment))
